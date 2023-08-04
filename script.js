c553258ba5b9430a3d474947a44f9806d9dc8303
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-08-04 15:45:43 -0400

    dynamically rendered js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let APIKey = "6f3fae3674ae2776180feb26321732b6";
 let queriedCity = "Pittsburgh";
 //event listener. 
 // pull out the string from the new button. use event delegation. event.target.textContent?
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${queriedCity}&limit=5&appid=6f3fae3674ae2776180feb26321732b6`)
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${queriedCity}&limit=5&appid=6f3fae3674ae2776180feb26321732b6&units=imperial`)
     .then(response => response.json())
     .then(cityInfo => {
         // let firstCity = citiesFound[0];
@@ -10,11 +10,37 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${queriedCity}&limit=5&a
         console.log(cityInfo.coord.lat);
         console.log(cityInfo.coord.lon);
 
-        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityInfo.coord.lat}&lon=${cityInfo.coord.lon}&appid=6f3fae3674ae2776180feb26321732b6`)
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "card");
+
+        let cardBody = document.createElement("div");
+        cardBody.setAttribute("class", "card-body");
+
+        let cityName = document.createElement("h5");
+        cityName.setAttribute("class", "card-title");
+        cityName.textContent= cityInfo.name;
+
+        let temp = document.createElement("p");
+        temp.setAttribute("class", "card-text");
+        temp.textContent= `Temperature: ${cityInfo.main.temp}F`
+        cardBody.append(cityName, temp);
+        newDiv.append(cardBody);
+        document.querySelector(".currentWeather").append(newDiv);
+
+
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityInfo.coord.lat}&lon=${cityInfo.coord.lon}&appid=6f3fae3674ae2776180feb26321732b6&units=imperial`)
     })
 
     .then(response => response.json())
     .then(cityData => {
 
         console.log(cityData);
+        for (let i = 0; i < cityData.list.length; i++) {
+            if (cityData.list[i].dt_txt.includes("12:00:00")) {
+                console.log(cityData.list[i]);
+            }
+            
+        }
     })
+
+
