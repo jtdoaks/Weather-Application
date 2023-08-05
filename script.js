b2513044e5109ca4826b7643e2f49232e6c9ba19
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-08-05 09:31:25 -0400

    trying to get the render of the pafge from a non working function

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function getWeather(queriedCity) {
         .then(cityInfo => {
             // let firstCity = citiesFound[0];
             console.log(cityInfo);
-            console.log(cityInfo.coord.lat);
-            console.log(cityInfo.coord.lon);
+            // console.log(cityInfo.coord.lat);
+            // console.log(cityInfo.coord.lon);
 
-            const newDiv = document.createElement("div");
+            let newDiv = document.createElement("div");
             newDiv.setAttribute("class", "card");
 
             let cardBody = document.createElement("div");
@@ -44,23 +44,22 @@ function getWeather(queriedCity) {
             cardBody.append(cityName, temp, wind, humidity);
             newDiv.append(cardBody);
 
-
-
             document.querySelector(".currentWeather").innerHTML = "";
             document.querySelector(".currentWeather").append(newDiv);
 
 
+            
             return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityInfo.coord.lat}&lon=${cityInfo.coord.lon}&appid=6f3fae3674ae2776180feb26321732b6&units=imperial`)
         })
 
         .then(response => response.json())
         .then(cityData => {
 
-            console.log(cityData);
+            // console.log(cityData);
             saveCity(cityData.city.name);
             for (let i = 0; i < cityData.list.length; i++) {
                 if (cityData.list[i].dt_txt.includes("12:00:00")) {
-                    console.log(cityData.list[i]);
+                    // console.log(cityData.list[i]);
 
                 }
 
@@ -78,22 +77,67 @@ function saveCity(citiesName) {
     }
 }
 
-
 function displayPastButtons() {
     let pastSearches = JSON.parse(localStorage.getItem("pastCities")) || [];
-     document.querySelector("#searchBox").innerHTML = "";
+    document.querySelector("#searchBox").innerHTML = "";
 
     for (let i = 0; i < pastSearches.length; i++) {
         let newButton = document.createElement("button");
         newButton.textContent = pastSearches[i];
         newButton.addEventListener("click", function (event) {
             let searchedCity = event.target.textContent;
-            console.log(searchedCity);
+            // console.log(searchedCity);
             getWeather(searchedCity);
+            
         })
         document.querySelector("#searchBox").append(newButton);
-
+        
     }
+    
 }
 
+
 displayPastButtons();
+// foreach()?
+            function displayFive(cityInfo) {
+                let fiveDayF = JSON.parse(localStorage.getItem("cityInfo")) || [];
+                fiveDayF.push(cityInfo);
+                localStorage.setItem("cityInfo", JSON.stringify(fiveDayF))
+                console.log(fiveDayF);
+
+
+                // document.querySelector("#fiveDay").innerHTML = "";
+
+
+                for (let i = 0; fiveDayF.length; i++) {
+                    // if (i > 3) break;
+          
+                    let days = document.createElement("div");
+                    days.setAttribute("class", "card");
+            
+                    let cardBody = document.createElement("div");
+                    cardBody.setAttribute("class", "card-body");
+            
+                    let date = document.createElement("div");
+                    date.setAttribute("class", "card");
+                    date.textContent = dayjs().format("DD/MM/YYYY") 
+            
+                    let temp = document.createElement("p");
+                    temp.setAttribute("class", "card-text");
+                    temp.textContent = `Temperature: ${cityInfo.main.temp} F`;
+            
+                    let wind = document.createElement("p");
+                    wind.setAttribute("class", "card-text");
+                    wind.textContent = `Wind: ${cityInfo.wind.speed} mph`;
+            
+                    let humidity = document.createElement("p");
+                    humidity.setAttribute("class", "card-text");
+                    humidity.textContent = `Humidity: ${cityInfo.main.humidity} %`;
+            
+                    cardBody.append(date, temp, wind, humidity);
+                    newDiv.append(cardBody);
+            
+                    // document.querySelector("#fiveDay").append(days);
+                }
+            }
+            displayFive();
